6299d52e834410c256f215d0450db00610aabc5c
Adam <adam@intelligenta.uk>
2026-05-23 15:29:23 +0100

    Cover stdio write encoding callbacks

--- tests/e2e/cases/process_stdio_write/in.ts
+++ tests/e2e/cases/process_stdio_write/in.ts
@@ -2,3 +2,21 @@ const outOk = process.stdout.write("raw:");
 const errOk = process.stderr.write("");
 
 console.log("write", outOk, errOk);
+
+let seen = "";
+function mark(label: string): string {
+    seen += label;
+    return "utf8";
+}
+function done(this: any): void {
+    seen += this === undefined ? "cb" : "bad";
+}
+const outWithCallback = process.stdout.write("enc:", mark("e"), done);
+console.log("callback", outWithCallback, seen);
+
+let errSeen = "";
+function errDone(): void {
+    errSeen = "err";
+}
+const errWithCallback = process.stderr.write("", errDone);
+console.log("stderr callback", errWithCallback, errSeen);
--- tests/e2e/cases/process_stdio_write_buffer/in.ts
+++ tests/e2e/cases/process_stdio_write_buffer/in.ts
@@ -2,3 +2,14 @@ const outOk = process.stdout.write(Buffer.from("buf:"));
 const errOk = process.stderr.write(Buffer.alloc(0));
 
 console.log("stdio buffer", outOk, errOk);
+
+let seen = "";
+function mark(label: string): string {
+    seen += label;
+    return "utf8";
+}
+function done(this: any): void {
+    seen += this === undefined ? "cb" : "bad";
+}
+const outWithCallback = process.stdout.write(Buffer.from("bufcb:"), mark("b"), done);
+console.log("buffer callback", outWithCallback, seen);
