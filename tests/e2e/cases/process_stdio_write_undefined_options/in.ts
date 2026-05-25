072f9cb162c17f85a32a80a388b787e28710df8a
Adam <adam@intelligenta.uk>
2026-05-25 01:41:14 +0100

    Cover void stdio write encodings

--- tests/e2e/cases/process_stdio_write_undefined_options/in.ts
+++ tests/e2e/cases/process_stdio_write_undefined_options/in.ts
@@ -4,9 +4,9 @@ function done(): void {
     callbacks++;
 }
 
-const outDefault = process.stdout.write("u:", undefined);
+const outDefault = process.stdout.write("u:", void 0);
 const outCallback = process.stdout.write("cb:", void 0, done);
-const outBuffer = process.stdout.write(Buffer.from("bufu:"), undefined, done);
+const outBuffer = process.stdout.write(Buffer.from("bufu:"), void 0, done);
 const errDefault = process.stderr.write("", void 0);
 const errCallback = process.stderr.write("", "utf8", void 0);
 
