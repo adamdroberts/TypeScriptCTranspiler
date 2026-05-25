26c47583688883478bef0cb86749c226fc2aa683
Adam <adam@intelligenta.uk>
2026-05-25 01:40:15 +0100

    Cover void Buffer encoding defaults

--- tests/e2e/cases/buffer_static_more/in.ts
+++ tests/e2e/cases/buffer_static_more/in.ts
@@ -6,7 +6,7 @@ function mark(label: string): string {
   return label;
 }
 
-console.log("byte length:", Buffer.byteLength("hello", undefined, mark("l")), Buffer.byteLength("4869", "hex", mark("h")), Buffer.byteLength(aa, "utf8", mark("b")));
+console.log("byte length:", Buffer.byteLength("hello", void 0, mark("l")), Buffer.byteLength("4869", "hex", mark("h")), Buffer.byteLength(aa, "utf8", mark("b")));
 console.log("encoding:", Buffer.isEncoding("utf8", mark("u")), Buffer.isEncoding("utf-8"), Buffer.isEncoding("hex"), Buffer.isEncoding("base64"));
 console.log("compare:", Buffer.compare(aa, ab, mark("c")), Buffer.compare(ab, aa), Buffer.compare(aa, Buffer.from("aa")));
 console.log("instance compare:", aa.compare(ab), ab.compare(aa), aa.compare(Buffer.from("aa")));
