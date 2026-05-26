1874ad4883aa9edeb545a460f8545cf01153f96c
Adam <adam@intelligenta.uk>
2026-05-26 07:49:57 +0100

    Cover Reflect target validation ordering

--- tests/e2e/cases/reflect_property_target_validation/in.ts
+++ tests/e2e/cases/reflect_property_target_validation/in.ts
@@ -1,26 +1,42 @@
 const badTarget: any = 1;
 const validTarget: any = { value: 1 };
+let trace = "";
+
+function mark(label: string): string {
+    trace += label;
+    return label;
+}
+
+function markValue(label: string): any {
+    trace += label;
+    return label;
+}
+
+function markDesc(label: string): any {
+    trace += label;
+    return { value: label, enumerable: true };
+}
 
 try {
-    console.log("get target:", Reflect.get(badTarget, "value"));
+    console.log("get target:", Reflect.get(badTarget, mark("g")));
 } catch (e: any) {
     console.log("get target:", e);
 }
 
 try {
-    console.log("set target:", Reflect.set(badTarget, "value", 2));
+    console.log("set target:", Reflect.set(badTarget, mark("s"), markValue("v")));
 } catch (e: any) {
     console.log("set target:", e);
 }
 
 try {
-    console.log("has target:", Reflect.has(badTarget, "value"));
+    console.log("has target:", Reflect.has(badTarget, mark("h")));
 } catch (e: any) {
     console.log("has target:", e);
 }
 
 try {
-    console.log("delete target:", Reflect.deleteProperty(badTarget, "value"));
+    console.log("delete target:", Reflect.deleteProperty(badTarget, mark("d")));
 } catch (e: any) {
     console.log("delete target:", e);
 }
@@ -32,16 +48,17 @@ try {
 }
 
 try {
-    console.log("gopd target:", Reflect.getOwnPropertyDescriptor(badTarget, "value"));
+    console.log("gopd target:", Reflect.getOwnPropertyDescriptor(badTarget, mark("p")));
 } catch (e: any) {
     console.log("gopd target:", e);
 }
 
 try {
-    console.log("define target:", Reflect.defineProperty(badTarget, "value", { value: 2 }));
+    console.log("define target:", Reflect.defineProperty(badTarget, mark("f"), markDesc("D")));
 } catch (e: any) {
     console.log("define target:", e);
 }
+console.log("trace:", trace);
 
 console.log("valid get:", Reflect.get(validTarget, "value"));
 console.log("valid set:", Reflect.set(validTarget, "value", 2), validTarget.value);
