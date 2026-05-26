9c505bf5950d7cd73fec3880e19d4db41ef5921e
Adam <adam@intelligenta.uk>
2026-05-26 07:57:30 +0100

    Cover Proxy constructor validation ordering

--- tests/e2e/cases/proxy_constructor_validation/in.ts
+++ tests/e2e/cases/proxy_constructor_validation/in.ts
@@ -1,20 +1,29 @@
+let trace = "";
+
+function mark(label: string, value: any): any {
+    trace += label;
+    return value;
+}
+
 try {
-    const badTarget: any = new Proxy(1 as any, {} as any);
+    const badTarget: any = new Proxy(mark("t", 1) as any, mark("h", {}) as any, mark("i", "ignored"));
     console.log("bad target:", badTarget);
 } catch (e: any) {
     console.log("bad target:", e);
 }
 
 try {
-    const badHandler: any = new Proxy({} as any, 1 as any);
+    const badHandler: any = new Proxy(mark("T", {}) as any, mark("H", 1) as any, mark("I", "ignored"));
     console.log("bad handler:", badHandler);
 } catch (e: any) {
     console.log("bad handler:", e);
 }
 
 try {
-    const badRevocable: any = Proxy.revocable("x" as any, {} as any);
+    const badRevocable: any = Proxy.revocable(mark("r", "x") as any, mark("R", {}) as any, mark("j", "ignored"));
     console.log("bad revocable:", badRevocable);
 } catch (e: any) {
     console.log("bad revocable:", e);
 }
+
+console.log("trace:", trace);
