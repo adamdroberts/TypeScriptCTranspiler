e9e3e1abb6ec6ea7be2e372f1ae9f6238615aaab
Adam <adam@intelligenta.uk>
2026-05-26 17:56:39 +0100

    Cover aliased child process imports

--- tests/e2e/cases/child_process_callback_ignored_arguments/in.ts
+++ tests/e2e/cases/child_process_callback_ignored_arguments/in.ts
@@ -1,5 +1,5 @@
 import childProcess from "node:child_process";
-import { exec, execFile } from "node:child_process";
+import { exec, exec as execAlias, execFile, execFile as execFileAlias } from "node:child_process";
 
 const events: string[] = [];
 
@@ -15,10 +15,12 @@ function record(label: string) {
 }
 
 exec("printf exec", record("exec"), mark("exec-extra"));
+execAlias("printf exec-alias", record("exec-alias"), mark("exec-alias-extra"));
 childProcess.exec("printf ns", { shell: "/bin/sh" }, record("exec-options"), mark("exec-options-extra"));
 
 execFile("/bin/true", record("file-callback"), mark("file-callback-extra"));
 execFile("/bin/printf", ["file"], record("file-args"), mark("file-args-extra"));
+execFileAlias("/bin/printf", ["file-alias"], record("file-alias"), mark("file-alias-extra"));
 execFile("/bin/true", { shell: "/bin/sh" }, record("file-options"), mark("file-options-extra"));
 childProcess.execFile("/bin/printf", ["file-opt"], { shell: false }, record("file-args-options"), mark("file-args-options-extra"));
 
--- tests/e2e/cases/child_process_sync_ignored_arguments/in.ts
+++ tests/e2e/cases/child_process_sync_ignored_arguments/in.ts
@@ -1,5 +1,5 @@
 import childProcess from "node:child_process";
-import { execFileSync, execSync, spawnSync } from "node:child_process";
+import { execFileSync, execFileSync as execFileSyncAlias, execSync, execSync as execSyncAlias, spawnSync, spawnSync as spawnSyncAlias } from "node:child_process";
 
 const events: string[] = [];
 
@@ -11,14 +11,20 @@ function mark(name: string): string {
 const UTF8_OPTIONS = { encoding: "utf8" } as const;
 
 const execOut = execSync("printf exec", UTF8_OPTIONS, mark("exec-extra"));
+const execAliasOut = execSyncAlias("printf alias-exec", UTF8_OPTIONS, mark("alias-exec-extra"));
 const fileOut = execFileSync("/bin/printf", ["file"], UTF8_OPTIONS, mark("file-extra"));
+const fileAliasOut = execFileSyncAlias("/bin/printf", ["alias-file"], UTF8_OPTIONS, mark("alias-file-extra"));
 const fileOptionsOut = childProcess.execFileSync("/bin/true", UTF8_OPTIONS, mark("file-options-extra"));
 const spawnOut: any = spawnSync("/bin/printf", ["spawn"], UTF8_OPTIONS, mark("spawn-extra"));
+const spawnAliasOut: any = spawnSyncAlias("/bin/printf", ["alias-spawn"], UTF8_OPTIONS, mark("alias-spawn-extra"));
 const spawnOptionsOut: any = childProcess.spawnSync("/bin/true", UTF8_OPTIONS, mark("spawn-options-extra"));
 
 console.log("exec:", execOut);
+console.log("exec-alias:", execAliasOut);
 console.log("file:", fileOut);
+console.log("file-alias:", fileAliasOut);
 console.log("file-options-length:", fileOptionsOut.length);
 console.log("spawn:", spawnOut.status, spawnOut.stdout);
+console.log("spawn-alias:", spawnAliasOut.status, spawnAliasOut.stdout);
 console.log("spawn-options:", spawnOptionsOut.status, spawnOptionsOut.stdout.length);
 console.log("events:", events.join("|"));
