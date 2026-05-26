6baee2256bead11b5f5235484e914e182fa72a20
Adam <adam@intelligenta.uk>
2026-05-26 07:42:36 +0100

    Cover accessor nullish argument evaluation

--- tests/e2e/cases/object_prototype_accessor_nullish_call/in.ts
+++ tests/e2e/cases/object_prototype_accessor_nullish_call/in.ts
@@ -5,6 +5,23 @@ function getter(): number {
 function setter(_value: number): void {
 }
 
+let trace = "";
+
+function mark(label: string): string {
+    trace += label;
+    return label;
+}
+
+function getGetter(): any {
+    trace += "G";
+    return getter;
+}
+
+function getSetter(): any {
+    trace += "S";
+    return setter;
+}
+
 function report(label: string, fn: () => any): void {
     try {
         console.log(label + ":", fn());
@@ -13,7 +30,8 @@ function report(label: string, fn: () => any): void {
     }
 }
 
-report("define getter null", (): any => Object.prototype.__defineGetter__.call(null, "x", getter));
-report("define setter undefined", (): any => Object.prototype.__defineSetter__.call(undefined, "x", setter));
-report("lookup getter null", (): any => Object.prototype.__lookupGetter__.call(null, "x"));
-report("lookup setter undefined", (): any => Object.prototype.__lookupSetter__.call(undefined, "x"));
+report("define getter null", (): any => Object.prototype.__defineGetter__.call(null, mark("g"), getGetter(), mark("i")));
+report("define setter undefined", (): any => Object.prototype.__defineSetter__.call(undefined, mark("s"), getSetter(), mark("j")));
+report("lookup getter null", (): any => Object.prototype.__lookupGetter__.call(null, mark("l"), mark("m")));
+report("lookup setter undefined", (): any => Object.prototype.__lookupSetter__.call(undefined, mark("L"), mark("M")));
+console.log("trace:", trace);
