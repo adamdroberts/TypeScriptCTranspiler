a43d4c6ea7e6e94e70ba46619ae6cb498e17bdc0
Adam <adam@intelligenta.uk>
2026-05-26 08:45:22 +0100

    Cover array proxy extensibility invariant ordering

--- tests/e2e/cases/proxy_array_extensibility_trap_invariants/in.ts
+++ tests/e2e/cases/proxy_array_extensibility_trap_invariants/in.ts
@@ -1,19 +1,29 @@
+const events: string[] = [];
+
+function mark(label: string): string {
+    events.push("ignored:" + label);
+    return label;
+}
+
 function falseIsExtensible(target: any): boolean {
+    events.push("is false:" + target.length);
     return false;
 }
 
 function trueIsExtensible(target: any): boolean {
+    events.push("is true:" + target.length);
     return true;
 }
 
 function truePreventExtensions(target: any): boolean {
+    events.push("prevent true:" + target.length);
     return true;
 }
 
 const openTarget: any = [1];
 const openMismatch: any = new Proxy(openTarget, { isExtensible: falseIsExtensible as any });
 try {
-    console.log("open false:", Object.isExtensible(openMismatch));
+    console.log("open false:", Object.isExtensible(openMismatch, mark("open false")));
 } catch (err: any) {
     console.log("open false:", err);
 }
@@ -22,7 +32,7 @@ const closedTarget: any = [2];
 Object.preventExtensions(closedTarget);
 const closedMismatch: any = new Proxy(closedTarget, { isExtensible: trueIsExtensible as any });
 try {
-    console.log("closed true:", Reflect.isExtensible(closedMismatch));
+    console.log("closed true:", Reflect.isExtensible(closedMismatch, mark("closed true")));
 } catch (err: any) {
     console.log("closed true:", err);
 }
@@ -30,7 +40,7 @@ try {
 const fakePreventTarget: any = [3];
 const fakePrevent: any = new Proxy(fakePreventTarget, { preventExtensions: truePreventExtensions as any });
 try {
-    console.log("fake prevent:", Reflect.preventExtensions(fakePrevent));
+    console.log("fake prevent:", Reflect.preventExtensions(fakePrevent, mark("fake prevent")));
 } catch (err: any) {
     console.log("fake prevent:", err);
 }
@@ -40,7 +50,9 @@ const realPreventTarget: any = [4];
 Object.preventExtensions(realPreventTarget);
 const realPrevent: any = new Proxy(realPreventTarget, { preventExtensions: truePreventExtensions as any });
 try {
-    console.log("real prevent:", Reflect.preventExtensions(realPrevent));
+    console.log("real prevent:", Reflect.preventExtensions(realPrevent, mark("real prevent")));
 } catch (err: any) {
     console.log("real prevent:", err);
 }
+
+console.log("events:", events.join("|"));
