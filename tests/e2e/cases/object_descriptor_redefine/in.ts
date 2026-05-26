7f36e56b11b42852ae13e8f9376af62390888088
Adam <adam@intelligenta.uk>
2026-05-26 09:56:08 +0100

    Cover descriptor SameValue redefinitions

--- tests/e2e/cases/object_descriptor_redefine/in.ts
+++ tests/e2e/cases/object_descriptor_redefine/in.ts
@@ -64,3 +64,32 @@ console.log(
     }),
     obj.x,
 );
+
+const plusZero: any = {};
+Object.defineProperty(plusZero, "fixed", {
+    value: 0,
+    writable: false,
+    enumerable: true,
+    configurable: false,
+});
+console.log("plus zero same:", Reflect.defineProperty(plusZero, "fixed", { value: 0 }), Object.is(plusZero.fixed, 0));
+console.log("plus zero negative:", Reflect.defineProperty(plusZero, "fixed", { value: -0 }), Object.is(plusZero.fixed, 0));
+
+const negativeZero: any = {};
+Object.defineProperty(negativeZero, "fixed", {
+    value: -0,
+    writable: false,
+    enumerable: true,
+    configurable: false,
+});
+console.log("negative zero same:", Reflect.defineProperty(negativeZero, "fixed", { value: -0 }), Object.is(negativeZero.fixed, -0));
+console.log("negative zero plus:", Reflect.defineProperty(negativeZero, "fixed", { value: 0 }), Object.is(negativeZero.fixed, -0));
+
+const nanValue: any = {};
+Object.defineProperty(nanValue, "fixed", {
+    value: NaN,
+    writable: false,
+    enumerable: true,
+    configurable: false,
+});
+console.log("nan same:", Reflect.defineProperty(nanValue, "fixed", { value: NaN }), Object.is(nanValue.fixed, NaN));
