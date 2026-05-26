e058cf50009cf86c0c782998464a5ea6dbd32a19
Adam <adam@intelligenta.uk>
2026-05-26 17:35:33 +0100

    Cover aliased buffer and console imports

--- tests/e2e/cases/buffer_base64_import/in.ts
+++ tests/e2e/cases/buffer_base64_import/in.ts
@@ -1,4 +1,4 @@
-import bufferDefault, { atob, btoa } from "node:buffer";
+import bufferDefault, { atob, atob as atobAlias, btoa, btoa as btoaAlias } from "node:buffer";
 import * as bufferNs from "buffer";
 
 let seen = "";
@@ -8,5 +8,6 @@ function mark(label: string): string {
 }
 
 console.log("named:", btoa("Hi"), atob("SGk="));
+console.log("alias:", btoaAlias("Az"), atobAlias("QXo="));
 console.log("namespace:", bufferNs.btoa("OK"), bufferNs.atob("T0s="));
 console.log("default:", bufferDefault.btoa("Yo", mark("b")), bufferDefault.atob("WW8=", mark("a")), seen);
--- tests/e2e/cases/console_module_import/in.ts
+++ tests/e2e/cases/console_module_import/in.ts
@@ -1,7 +1,9 @@
-import consoleDefault, { info, log } from "node:console";
+import consoleDefault, { info, info as infoAlias, log, log as logAlias } from "node:console";
 import * as consoleNs from "console";
 
 log("named %s", "log");
 info("named info", 1);
+logAlias("alias %s", "log");
+infoAlias("alias info", 3);
 consoleNs.log("namespace %d", 2);
 consoleDefault.info("default", ["ok"]);
