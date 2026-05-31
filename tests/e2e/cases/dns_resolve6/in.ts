6c9f479974eb86b23f37e0a37fe2b67935b34018
Adam <adam@intelligenta.uk>
2026-05-31 05:23:33 +0100

    Add dns resolve6 helpers [skip ci]

---
+++ tests/e2e/cases/dns_resolve6/in.ts
@@ -0,0 +1,28 @@
+import * as dns from "node:dns";
+import { resolve6 } from "dns";
+import { promises as dnsPromises } from "node:dns";
+import { resolve6 as resolve6Promises } from "node:dns/promises";
+
+// Test callback form with node:dns
+dns.resolve6("::1", (err: any, addresses: string[]): void => {
+    console.log("callback ::1:", err === null, addresses.length > 0 ? addresses[0] : "empty");
+});
+
+// Test callback form with direct import from dns
+resolve6("::1", (err: any, addresses: string[]): void => {
+    console.log("callback direct ::1:", err === null, addresses.length > 0 ? addresses[0] : "empty");
+});
+
+// Test promise form with node:dns
+dnsPromises.resolve6("::1").then((addresses: string[]): void => {
+    console.log("promise ::1:", addresses.length > 0 ? addresses[0] : "empty");
+}).catch((err: any): void => {
+    console.log("promise ::1 error:", err);
+});
+
+// Test promise form with direct import from node:dns/promises
+resolve6Promises("::1").then((addresses: string[]): void => {
+    console.log("promise direct ::1:", addresses.length > 0 ? addresses[0] : "empty");
+}).catch((err: any): void => {
+    console.log("promise direct ::1 error:", err);
+});
