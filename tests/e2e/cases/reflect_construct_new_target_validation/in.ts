3577685434071500f5eae64f74987224d954b027
Adam <adam@intelligenta.uk>
2026-05-31 22:09:13 +0100

    fix: reject accessor functions as constructors [skip ci]

--- tests/e2e/cases/reflect_construct_new_target_validation/in.ts
+++ tests/e2e/cases/reflect_construct_new_target_validation/in.ts
@@ -10,6 +10,13 @@ function trapConstruct(target: any, args: any, newTarget: any): any {
     return { reached: true };
 }
 
+const events: string[] = [];
+
+function eventTrapConstruct(target: any, args: any, newTarget: any): any {
+    events.push("construct trap");
+    return { reached: true };
+}
+
 const target: any = Target as any;
 const badNewTarget: any = { name: "bad" };
 
@@ -28,3 +35,36 @@ try {
 
 const valid: any = Reflect.construct(target, [], Other as any);
 console.log("valid:", typeof valid, valid.value);
+
+const accessorSource: any = {};
+Object.defineProperty(accessorSource, "value", {
+    get: function(): any {
+        return 1;
+    },
+    set: function(value: any): void {
+        events.push("set:" + value);
+    },
+});
+
+const descriptor: any = Object.getOwnPropertyDescriptor(accessorSource, "value");
+const getterProxy: any = new Proxy(descriptor.get, { construct: eventTrapConstruct as any });
+try {
+    console.log("getter target:", Reflect.construct(getterProxy, []));
+} catch (e: any) {
+    console.log("getter target:", e);
+}
+
+const setterProxy: any = new Proxy(descriptor.set, { construct: eventTrapConstruct as any });
+try {
+    console.log("setter target:", Reflect.construct(setterProxy, []));
+} catch (e: any) {
+    console.log("setter target:", e);
+}
+
+try {
+    console.log("getter newTarget:", Reflect.construct(target, [], getterProxy));
+} catch (e: any) {
+    console.log("getter newTarget:", e);
+}
+
+console.log("events:" + events.join("|"));
