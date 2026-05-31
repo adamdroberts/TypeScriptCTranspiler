788bb9c8a65f2b4a3a3093b7d74f22f5401efc2b
Adam <adam@intelligenta.uk>
2026-05-31 02:05:53 +0100

    Support dynamic Symbol.iterator calls

---
+++ tests/e2e/cases/generator_dynamic_symbol_iterator/in.ts
@@ -0,0 +1,19 @@
+function* range(start: number, end: number): IterableIterator<number> {
+    for (let value = start; value <= end; value++) {
+        yield value;
+    }
+}
+
+const gen: any = range(10, 12);
+const genIter: any = gen[Symbol.iterator]();
+console.log("gen next 1:", genIter.next().value);
+console.log("gen next 2:", genIter.next().value);
+console.log("gen next 3:", genIter.next().value);
+console.log("gen next 4 done:", genIter.next().done);
+
+const str: any = "xyz";
+const strIter: any = str[Symbol.iterator]();
+console.log("str next 1:", strIter.next().value);
+console.log("str next 2:", strIter.next().value);
+console.log("str next 3:", strIter.next().value);
+console.log("str next 4 done:", strIter.next().done);
