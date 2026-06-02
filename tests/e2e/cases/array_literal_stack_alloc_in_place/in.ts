bea0a64fbb5ccef5ba948e08ba14558c49193cfd
Adam <adam@intelligenta.uk>
2026-06-02 12:05:41 +0100

    [skip ci] Allow safe wrapped array mutator results

--- tests/e2e/cases/array_literal_stack_alloc_in_place/in.ts
+++ tests/e2e/cases/array_literal_stack_alloc_in_place/in.ts
@@ -6,4 +6,13 @@ function total(): number {
     return values[0] + values[1] + values[2];
 }
 
-console.log(total());
+function wrappedUses(): string {
+    const checks: number[] = [4, 2, 1];
+    const direct = Array.isArray(checks.reverse());
+    const keys = Object.keys(checks.fill(9, 1, 2)).join("|");
+    const same = Object.prototype.valueOf.call((checks.copyWithin(2, 0, 1))).join("|");
+    ((checks.sort()));
+    return [direct, keys, same, checks.join("|")].join(":");
+}
+
+console.log(total(), wrappedUses());
