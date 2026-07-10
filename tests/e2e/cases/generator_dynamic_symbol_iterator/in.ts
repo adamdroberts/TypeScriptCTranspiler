e93110961a46085ac0bd463154d823bbc37cb8cd
Adam <adam@intelligenta.uk>
2026-07-10 17:14:37 +0100

    fix: mark generator methods nonconstructable [skip ci]

--- tests/e2e/cases/generator_dynamic_symbol_iterator/in.ts
+++ tests/e2e/cases/generator_dynamic_symbol_iterator/in.ts
@@ -6,6 +6,14 @@ function* range(start: number, end: number): IterableIterator<number> {
 
 const gen: any = range(10, 12);
 const genIter: any = gen[Symbol.iterator]();
+const nextMethod: any = genIter.next;
+console.log("gen method:", nextMethod.name, nextMethod.length, Object.hasOwn(nextMethod, "prototype"));
+try {
+    Reflect.construct(nextMethod, []);
+    console.log("gen construct:", "ok");
+} catch (err: any) {
+    console.log("gen construct:", err);
+}
 console.log("gen next 1:", genIter.next().value);
 console.log("gen next 2:", genIter.next().value);
 console.log("gen next 3:", genIter.next().value);
