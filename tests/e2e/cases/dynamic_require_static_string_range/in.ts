5062eee0fa696873ecde32954c7f2bdd22bb1d14
Adam <adam@intelligenta.uk>
2026-07-18 14:42:44 +0100

    Support string substr require proofs

--- tests/e2e/cases/dynamic_require_static_string_range/in.ts
+++ tests/e2e/cases/dynamic_require_static_string_range/in.ts
@@ -3,10 +3,12 @@ const sliceSource = "xx./slice_tailzz";
 const substringSource = "xx./substringyy";
 const swappedSource = "xx./swapyy";
 const undefSource = "xx./slice_undef";
+const substrSource = "xx./substr_tailzz";
 
 const sliceTail = require(sliceSource.slice(2, -2));
 const substring = require(substringSource.substring(2, 13));
 const swapped = require(swappedSource.substring(8, 2));
 const undef = require(undefSource.slice(2, undefined));
+const substrTail = require(substrSource.substr(2, 13));
 
-console.log(sliceTail.label, substring.label, swapped.label, undef.label);
+console.log(sliceTail.label, substring.label, swapped.label, undef.label, substrTail.label);
