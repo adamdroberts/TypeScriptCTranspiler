7f0a3db6cc5a21b89118e7954ff1fccf1ac77cce
Adam <adam@intelligenta.uk>
2026-07-18 19:56:20 +0100

    Support Buffer object entries require proofs

---
+++ tests/e2e/cases/dynamic_require_static_buffer_object_entries/in.ts
@@ -0,0 +1,6 @@
+const entryKey0 = require("./boe_" + Object.entries(Buffer.from("Hi"))[0][0]);
+const entryValue0 = require("./boe_" + Object.entries(Buffer.from("Hi"))[0][1]);
+const entryKey1 = require("./boe_" + Object.entries(Buffer.from("Hi"))[1][0]);
+const entryValue1 = require("./boe_" + Object.entries(Buffer.from("Hi"))[1][1]);
+
+console.log(entryKey0.label, entryValue0.label, entryKey1.label, entryValue1.label);
