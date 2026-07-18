70af0e77b55a3ce2212ea24561decc7153e8b7c9
Adam <adam@intelligenta.uk>
2026-07-18 14:39:06 +0100

    Support string bracket require proofs

--- tests/e2e/cases/dynamic_require_static_string_index/in.ts
+++ tests/e2e/cases/dynamic_require_static_string_index/in.ts
@@ -1,5 +1,7 @@
 // @ts-nocheck: dynamic require proof coverage intentionally exercises string indexing calls.
 const charAt = require("./" + "alpha".charAt(0));
 const at = require("./" + "bravo".at(-1));
+const bracketIndex = 0;
+const bracket = require("./" + "charlie"[bracketIndex]);
 
-console.log(charAt.label, at.label);
+console.log(charAt.label, at.label, bracket.label);
