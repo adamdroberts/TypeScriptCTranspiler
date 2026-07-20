6a8ec1b6d7cbe0f6ecc87b1e0fc67e84a5954968
Adam <adam@intelligenta.uk>
2026-07-20 03:50:33 +0100

    Cover branch try finally throws

---
+++ tests/e2e/cases/async_await_branch_block_source_try_finally_throw_five_await/in.ts
@@ -0,0 +1,49 @@
+import { setTimeout as delay } from "node:timers/promises";
+
+function delayedRejectAfter(ms: number, reason: string): Promise<string> {
+    return delay(ms, reason).then((value: string): string => {
+        throw value;
+    });
+}
+
+async function declaration(flag: boolean): Promise<string> {
+    if (flag) {
+        try {
+            throw (await delay(1, "one") + await delay(1, "two") + await delay(1, "three") + await delay(1, "four") + await delay(1, "five"));
+        } finally {
+            console.log("finally: declaration");
+        }
+    }
+    return "declaration-fallthrough";
+}
+
+class Worker {
+    async run(flag: boolean): Promise<string> {
+        if (flag) {
+            try {
+                throw (await delayedRejectAfter(1, "method-rejection") + await delay(1, "unused-two") + await delay(1, "unused-three") + await delay(1, "unused-four") + await delay(1, "unused-five"));
+            } finally {
+                console.log("finally: method");
+            }
+        }
+        return "method-fallthrough";
+    }
+}
+
+const value = async (flag: boolean): Promise<string> => {
+    if (flag) {
+        try {
+            throw (await delay(1, "arrow-one") + await delay(1, "arrow-two") + await delay(1, "arrow-three") + await delay(1, "arrow-four") + await delay(1, "arrow-five"));
+        } finally {
+            console.log("finally: arrow");
+        }
+    }
+    return "arrow-fallthrough";
+};
+
+declaration(true).catch((reason) => console.log("declaration-true:", reason));
+declaration(false).then((result) => console.log("declaration-false:", result));
+new Worker().run(true).catch((reason) => console.log("method-true:", reason));
+new Worker().run(false).then((result) => console.log("method-false:", result));
+value(true).catch((reason) => console.log("value-true:", reason));
+value(false).then((result) => console.log("value-false:", result));
