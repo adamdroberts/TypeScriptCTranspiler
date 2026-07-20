489c1520b1f4c11fd6b80218022ca740a5e949a6
Adam <adam@intelligenta.uk>
2026-07-20 11:00:58 +0100

    Cover awaited if conditions across async contexts

--- tests/e2e/cases/async_await_if_condition/in.ts
+++ tests/e2e/cases/async_await_if_condition/in.ts
@@ -5,3 +5,20 @@ async function choose(flag: boolean): Promise<string> {
 
 choose(true).then((value) => console.log("await-if-true", value));
 choose(false).then((value) => console.log("await-if-false", value));
+
+class Chooser {
+    async pick(flag: boolean): Promise<string> {
+        if (await Promise.resolve(flag)) return "method-yes";
+        return "method-no";
+    }
+}
+
+const chooseValue = async (flag: boolean): Promise<string> => {
+    if (await Promise.resolve(flag)) return "value-yes";
+    return "value-no";
+};
+
+new Chooser().pick(true).then((value) => console.log("await-if-method-true", value));
+new Chooser().pick(false).then((value) => console.log("await-if-method-false", value));
+chooseValue(true).then((value) => console.log("await-if-value-true", value));
+chooseValue(false).then((value) => console.log("await-if-value-false", value));
