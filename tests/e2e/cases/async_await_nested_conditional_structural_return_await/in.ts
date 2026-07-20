d26fb1b3fad233f6ca3a7fe41c806e417aeab7de
Adam <adam@intelligenta.uk>
2026-07-20 04:49:20 +0100

    Cover conditional structural return await

---
+++ tests/e2e/cases/async_await_nested_conditional_structural_return_await/in.ts
@@ -0,0 +1,31 @@
+import { setTimeout as delay } from "node:timers/promises";
+
+function combine(left: string, right: string): string {
+    return left + ":" + right;
+}
+
+async function declaration(flag: boolean): Promise<any> {
+    return await (flag
+        ? combine(await delay(1, "declaration-left"), await delay(1, "declaration-right"))
+        : { first: await delay(1, "declaration-first"), second: await delay(1, "declaration-second") });
+}
+
+class Worker {
+    async run(flag: boolean): Promise<any> {
+        return await (flag
+            ? combine(await delay(1, "method-left"), await delay(1, "method-right"))
+            : `method-${await delay(1, "method-first")}-${await delay(1, "method-second")}`);
+    }
+}
+
+const value = async (flag: boolean): Promise<any> =>
+    await (flag
+        ? `arrow-${await delay(1, "arrow-left")}-${await delay(1, "arrow-right")}`
+        : { first: await delay(1, "arrow-first"), second: await delay(1, "arrow-second") });
+
+declaration(true).then((result) => console.log("declaration-true:", result));
+declaration(false).then((result) => console.log("declaration-false:", JSON.stringify(result)));
+new Worker().run(true).then((result) => console.log("method-true:", result));
+new Worker().run(false).then((result) => console.log("method-false:", result));
+value(true).then((result) => console.log("value-true:", result));
+value(false).then((result) => console.log("value-false:", JSON.stringify(result)));
