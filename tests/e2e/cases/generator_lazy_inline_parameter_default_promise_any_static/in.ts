25e27fcfbb3f137a66a64d04a3708d487e4d5568
Adam <adam@intelligenta.uk>
2026-07-22 23:06:01 +0100

    Support lazy Promise.any defaults

---
+++ tests/e2e/cases/generator_lazy_inline_parameter_default_promise_any_static/in.ts
@@ -0,0 +1,23 @@
+function* values(): Generator<number, number, number> {
+    return (yield 1) + ((winner: Promise<number> = Promise.any([Promise.resolve(1), Promise.resolve(2)]), valid: boolean = winner === winner) => valid ? 1 : 0)() + (yield 2);
+}
+
+const iter = values();
+const first: any = iter.next();
+const second: any = iter.next(3);
+const done: any = iter.next(4);
+console.log(first.value, second.value, done.done, done.value);
+
+function makeCapturedValues(): () => Generator<number, number, number> {
+    let offset = 5;
+    offset = 6;
+    return function* (): Generator<number, number, number> {
+        return (yield 1) + ((winner: Promise<number> = Promise.any([Promise.resolve(1), Promise.resolve(2)]), valid: boolean = winner === winner) => (valid ? 1 : 0) + offset)() + (yield 2);
+    };
+}
+
+const capturedIter = makeCapturedValues()();
+const capturedFirst: any = capturedIter.next();
+const capturedSecond: any = capturedIter.next(3);
+const capturedDone: any = capturedIter.next(4);
+console.log(capturedFirst.value, capturedSecond.value, capturedDone.done, capturedDone.value);
