91e8daf31d1e7ef894967f84d784380564e24583
Adam <adam@intelligenta.uk>
2026-07-22 01:10:59 +0100

    Cover do-while awaited if throws

---
+++ tests/e2e/cases/async_await_do_body_await_if_throw/in.ts
@@ -0,0 +1,38 @@
+function later(value: string): Promise<string> {
+    return new Promise<string>((resolve) => setImmediate(() => resolve(value)));
+}
+
+function laterCondition(value: boolean): Promise<boolean> {
+    return new Promise<boolean>((resolve) => setImmediate(() => resolve(value)));
+}
+
+export {};
+
+let bodyCount = 0;
+
+function laterBody(value: string): Promise<string> {
+    bodyCount++;
+    return later(value);
+}
+
+async function doBodyAwaitIfThrow(): Promise<string> {
+    let count = 0;
+    do {
+        if (await laterCondition(count === 0)) {
+            await laterBody("continue-" + count);
+            count++;
+            continue;
+        } else {
+            await later("prep-a-" + count);
+            await later("prep-b-" + count);
+            bodyCount += 0;
+            throw await later("boom-" + bodyCount);
+        }
+    } while (await laterCondition(count < 3));
+    return await later("done-" + bodyCount);
+}
+
+doBodyAwaitIfThrow().then(
+    (value) => console.log(value),
+    (error) => console.log("error:" + error),
+);
