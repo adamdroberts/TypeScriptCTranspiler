13ca220398490bd57bdb3732b6601c7fe0a5e3a2
Adam <adam@intelligenta.uk>
2026-07-22 15:57:29 +0100

    Cover do while interstitial await chains

--- tests/e2e/cases/async_await_eight_step_chain/in.ts
+++ tests/e2e/cases/async_await_eight_step_chain/in.ts
@@ -15,6 +15,9 @@ async function declaration(): Promise<string> {
     while (loopMarker.length > 0) {
         loopMarker = "";
     }
+    do {
+        loopMarker = "";
+    } while (loopMarker.length > 0);
     const two = await delay(2, one + "2");
     const three = await delay(3, two + "3");
     const four = await delay(4, three + "4");
@@ -47,6 +50,9 @@ class Chain {
         while (loopMarker.length > 0) {
             loopMarker = "";
         }
+        do {
+            loopMarker = "";
+        } while (loopMarker.length > 0);
         const two = await delay(2, one + "b");
         const three = await delay(3, two + "c");
         const four = await delay(4, three + "d");
@@ -79,6 +85,9 @@ const value = async (): Promise<string> => {
     while (loopMarker.length > 0) {
         loopMarker = "";
     }
+    do {
+        loopMarker = "";
+    } while (loopMarker.length > 0);
     const two = await delay(2, one + "B");
     const three = await delay(3, two + "C");
     const four = await delay(4, three + "D");
@@ -111,6 +120,9 @@ async function branchEight(flag: boolean): Promise<string> {
         while (loopMarker.length > 0) {
             loopMarker = "";
         }
+        do {
+            loopMarker = "";
+        } while (loopMarker.length > 0);
         const two = await delay(22, one + "1");
         const three = await delay(23, two + "2");
         const four = await delay(24, three + "3");
@@ -140,6 +152,9 @@ async function branchEight(flag: boolean): Promise<string> {
     while (loopMarker.length > 0) {
         loopMarker = "";
     }
+    do {
+        loopMarker = "";
+    } while (loopMarker.length > 0);
     const two = await delay(30, one + "1");
     const three = await delay(31, two + "2");
     const four = await delay(32, three + "3");
@@ -173,6 +188,9 @@ class BranchChain {
             while (loopMarker.length > 0) {
                 loopMarker = "";
             }
+            do {
+                loopMarker = "";
+            } while (loopMarker.length > 0);
             const two = await delay(38, one + "1");
             const three = await delay(39, two + "2");
             const four = await delay(40, three + "3");
@@ -202,6 +220,9 @@ class BranchChain {
         while (loopMarker.length > 0) {
             loopMarker = "";
         }
+        do {
+            loopMarker = "";
+        } while (loopMarker.length > 0);
         const two = await delay(46, one + "1");
         const three = await delay(47, two + "2");
         const four = await delay(48, three + "3");
@@ -235,6 +256,9 @@ const branchValue = async (flag: boolean): Promise<string> => {
         while (loopMarker.length > 0) {
             loopMarker = "";
         }
+        do {
+            loopMarker = "";
+        } while (loopMarker.length > 0);
         const two = await delay(54, one + "1");
         const three = await delay(55, two + "2");
         const four = await delay(56, three + "3");
@@ -264,6 +288,9 @@ const branchValue = async (flag: boolean): Promise<string> => {
     while (loopMarker.length > 0) {
         loopMarker = "";
     }
+    do {
+        loopMarker = "";
+    } while (loopMarker.length > 0);
     const two = await delay(62, one + "1");
     const three = await delay(63, two + "2");
     const four = await delay(64, three + "3");
