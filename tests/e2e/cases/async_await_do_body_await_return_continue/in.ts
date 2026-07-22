901232b0901b21d364e6c8fc8b32da1aa478ab98
Adam <adam@intelligenta.uk>
2026-07-22 01:14:20 +0100

    Cover do-while awaited return continues

---
+++ tests/e2e/cases/async_await_do_body_await_return_continue/in.ts
@@ -0,0 +1,32 @@
+function later(value: string): Promise<string> {
+    return new Promise<string>((resolve) => setImmediate(() => resolve(value)));
+}
+
+function laterCondition(value: boolean): Promise<boolean> {
+    return new Promise<boolean>((resolve) => setImmediate(() => resolve(value)));
+}
+
+export {};
+
+let bodyCount = 0;
+
+function laterBody(value: string): Promise<string> {
+    bodyCount++;
+    return later(value);
+}
+
+async function doBodyAwaitReturnContinue(): Promise<string> {
+    let count = 0;
+    do {
+        if (await laterCondition(count !== 0)) {
+            return await later("early-" + bodyCount);
+        } else {
+            await laterBody("continue-" + count);
+            count++;
+            continue;
+        }
+    } while (await laterCondition(count < 3));
+    return await later("done-" + bodyCount);
+}
+
+doBodyAwaitReturnContinue().then((value) => console.log(value));
