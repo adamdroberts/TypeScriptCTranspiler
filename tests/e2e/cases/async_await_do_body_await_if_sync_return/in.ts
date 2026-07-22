03c5cbc7c35616d7f3cbb3777c83e588b89d75a4
Adam <adam@intelligenta.uk>
2026-07-22 01:28:46 +0100

    Cover do-while synchronous returns

---
+++ tests/e2e/cases/async_await_do_body_await_if_sync_return/in.ts
@@ -0,0 +1,35 @@
+function later(value: string): Promise<string> {
+    return new Promise<string>((resolve) => setImmediate(() => resolve(value)));
+}
+
+function laterCondition(value: boolean): Promise<boolean> {
+    return new Promise<boolean>((resolve) => setImmediate(() => resolve(value)));
+}
+
+export {};
+
+let bodyCount = 0;
+
+function laterBody(value: string): Promise<string> {
+    bodyCount++;
+    return later(value);
+}
+
+async function doBodyAwaitIfSyncReturn(): Promise<string> {
+    let count = 0;
+    do {
+        if (await laterCondition(count !== 0)) {
+            await later("prep-a-" + count);
+            await later("prep-b-" + count);
+            bodyCount += 0;
+            return "early-" + bodyCount;
+        } else {
+            await laterBody("continue-" + count);
+            count++;
+            continue;
+        }
+    } while (await laterCondition(count < 3));
+    return await later("done-" + bodyCount);
+}
+
+doBodyAwaitIfSyncReturn().then((value) => console.log(value));
