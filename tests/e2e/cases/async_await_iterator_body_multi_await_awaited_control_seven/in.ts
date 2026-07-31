734951f52932265d79af7989a51e621081fbaca4
Adam <adam@intelligenta.uk>
2026-08-01 00:59:23 +0100

    Cover seven-await iterator control arms

--- tests/e2e/cases/async_await_iterator_body_multi_await_awaited_control_seven/in.ts
+++ tests/e2e/cases/async_await_iterator_body_multi_await_awaited_control_seven/in.ts
@@ -41,9 +41,11 @@ async function runOf(): Promise<string> {
             await laterOfControl(lastOf);
             await laterOfControl(lastOf);
             await laterOfControl(lastOf);
+            await laterOfControl(lastOf);
             continue;
         } else {
             lastOf = second;
+            await laterOfControl(lastOf);
             await laterOfControl(lastOf);
             await laterOfControl(lastOf);
             await laterOfControl(lastOf);
@@ -69,9 +71,11 @@ async function runIn(): Promise<string> {
             await laterInControl(lastIn);
             await laterInControl(lastIn);
             await laterInControl(lastIn);
+            await laterInControl(lastIn);
             continue;
         } else {
             lastIn = second;
+            await laterInControl(lastIn);
             await laterInControl(lastIn);
             await laterInControl(lastIn);
             await laterInControl(lastIn);
--- tests/e2e/cases/async_await_iterator_body_multi_await_awaited_switch_seven/in.ts
+++ tests/e2e/cases/async_await_iterator_body_multi_await_awaited_switch_seven/in.ts
@@ -36,9 +36,11 @@ async function runOf(): Promise<string> {
                 await laterControl(lastOf);
                 await laterControl(lastOf);
                 await laterControl(lastOf);
+                await laterControl(lastOf);
                 continue;
             default:
                 lastOf = second;
+                await laterControl(lastOf);
                 await laterControl(lastOf);
                 await laterControl(lastOf);
                 await laterControl(lastOf);
@@ -65,9 +67,11 @@ async function runIn(): Promise<string> {
                 await laterControl(lastIn);
                 await laterControl(lastIn);
                 await laterControl(lastIn);
+                await laterControl(lastIn);
                 continue;
             default:
                 lastIn = second;
+                await laterControl(lastIn);
                 await laterControl(lastIn);
                 await laterControl(lastIn);
                 await laterControl(lastIn);
