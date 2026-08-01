8a46664ec52a4918f5550f4df938fabe085fb5a8
Adam <adam@intelligenta.uk>
2026-08-01 01:23:06 +0100

    Cover fourteen-await iterator control arms

--- tests/e2e/cases/async_await_iterator_body_multi_await_awaited_control_fourteen/in.ts
+++ tests/e2e/cases/async_await_iterator_body_multi_await_awaited_control_fourteen/in.ts
@@ -48,9 +48,11 @@ async function runOf(): Promise<string> {
             await laterOfControl(lastOf);
             await laterOfControl(lastOf);
             await laterOfControl(lastOf);
+            await laterOfControl(lastOf);
             continue;
         } else {
             lastOf = second;
+            await laterOfControl(lastOf);
             await laterOfControl(lastOf);
             await laterOfControl(lastOf);
             await laterOfControl(lastOf);
@@ -90,9 +92,11 @@ async function runIn(): Promise<string> {
             await laterInControl(lastIn);
             await laterInControl(lastIn);
             await laterInControl(lastIn);
+            await laterInControl(lastIn);
             continue;
         } else {
             lastIn = second;
+            await laterInControl(lastIn);
             await laterInControl(lastIn);
             await laterInControl(lastIn);
             await laterInControl(lastIn);
