cd120c3be3572275ed21f9581001d60937162e85
Adam <adam@intelligenta.uk>
2026-08-02 05:42:05 +0100

    Support parse number wrapped awaited selectors

--- src/emit/index.ts
+++ src/emit/index.ts
@@ -33208,7 +33208,7 @@ class Emitter {
             return null;
         }
         const name = expression.expression.text;
-        if ((name !== "AggregateError" && name !== "Array" && name !== "BigInt" && name !== "Boolean" && name !== "Date" && name !== "Error" && name !== "Number" && name !== "Object" && name !== "RegExp" && name !== "String" && name !== "Symbol") ||
+        if ((name !== "AggregateError" && name !== "Array" && name !== "BigInt" && name !== "Boolean" && name !== "Date" && name !== "Error" && name !== "Number" && name !== "Object" && name !== "RegExp" && name !== "String" && name !== "Symbol" && name !== "parseFloat" && name !== "parseInt") ||
             !this.isUnshadowedGlobalIdentifier(expression.expression, name)) {
             return null;
         }
@@ -33225,7 +33225,7 @@ class Emitter {
             return null;
         }
         const name = expression.expression.text;
-        if ((name !== "AggregateError" && name !== "Array" && name !== "BigInt" && name !== "Boolean" && name !== "Date" && name !== "Error" && name !== "Number" && name !== "Object" && name !== "RegExp" && name !== "String" && name !== "Symbol") ||
+        if ((name !== "AggregateError" && name !== "Array" && name !== "BigInt" && name !== "Boolean" && name !== "Date" && name !== "Error" && name !== "Number" && name !== "Object" && name !== "RegExp" && name !== "String" && name !== "Symbol" && name !== "parseFloat" && name !== "parseInt") ||
             !this.isUnshadowedGlobalIdentifier(expression.expression, name)) {
             return null;
         }
