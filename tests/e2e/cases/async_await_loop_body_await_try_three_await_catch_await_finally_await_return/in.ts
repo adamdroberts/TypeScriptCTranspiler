97066fc2a86a0deb4c0f852bbb14377239b4abf2
Adam <adam@intelligenta.uk>
2026-08-04 04:10:40 +0100

    Prove three-stage loop source awaits

---
+++ tests/e2e/cases/async_await_loop_body_await_try_three_await_catch_await_finally_await_return/in.ts
@@ -0,0 +1,49 @@
+function later(value: string): Promise<string> {
+    return new Promise<string>((resolve) => setImmediate(() => resolve(value)));
+}
+
+function rejectedLater(value: string): Promise<string> {
+    return new Promise<string>((resolve, reject) => setImmediate(() => reject(value)));
+}
+
+function laterCondition(value: boolean): Promise<boolean> {
+    return new Promise<boolean>((resolve) => setImmediate(() => resolve(value)));
+}
+
+let count = 0;
+let firstStage = 0;
+let secondStage = 0;
+let thirdStage = 0;
+let recovered = 0;
+let cleaned = 0;
+
+function recover(value: string): Promise<string> {
+    recovered++;
+    return later(value);
+}
+
+function cleanup(value: string): Promise<string> {
+    cleaned++;
+    return later(value);
+}
+
+async function run(): Promise<string> {
+    while (await laterCondition(count < 1)) {
+        try {
+            await later("first");
+            firstStage++;
+            await later("second");
+            secondStage++;
+            await rejectedLater("third");
+            thirdStage++;
+        } catch (reason) {
+            await recover("recover-" + reason);
+        } finally {
+            await cleanup("cleanup");
+        }
+        return await later(firstStage + "-" + secondStage + "-" + thirdStage + "-" + recovered + "-" + cleaned);
+    }
+    return await later("done");
+}
+
+run().then((value) => console.log(value));
