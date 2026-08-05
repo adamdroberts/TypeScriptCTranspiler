e3bdb091aa1697bc1e2ae13439686102a3af9f5c
Adam <adam@intelligenta.uk>
2026-08-05 20:57:42 +0100

    Cover nested template logical trees

---
+++ tests/e2e/cases/generator_lazy_for_multi_yield_push_unshift_optional_call_nested_template_logical_tree_yield_argument_access_declaration_initializer/in.ts
@@ -0,0 +1,85 @@
+function pushNestedTemplateLogicalTreeCallback(value: any): any {
+    return "push-tree-" + value;
+}
+
+function unshiftNestedTemplateLogicalTreeCallback(value: any): any {
+    return "unshift-tree-" + value;
+}
+
+function* multiYieldPushNestedTemplateLogicalTreeArgumentAccessDeclarationInitializer(): Generator<string, string, any> {
+    for (
+        let value: any = (yield "push-receiver").push(
+            pushNestedTemplateLogicalTreeCallback?.(`push-${((yield "push-outer") && ((yield "push-inner-left") || (yield "push-inner-right")))}-${yield "push-tail"}`),
+            yield "push-item",
+            ...(yield "push-spread")
+        )[yield "push-key"],
+        count = 0;
+        count < 1;
+        count++
+    ) {
+        yield "push-body-" + typeof value;
+    }
+    return "push-done";
+}
+
+function* multiYieldUnshiftNestedTemplateLogicalTreeArgumentAccessDeclarationInitializer(): Generator<string, string, any> {
+    for (
+        let value: any = (yield "unshift-receiver").unshift(
+            unshiftNestedTemplateLogicalTreeCallback?.(String.raw`unshift-${((yield "unshift-outer") ?? ((yield "unshift-inner-left") && (yield "unshift-inner-right")))}-${yield "unshift-tail"}`),
+            yield "unshift-item",
+            ...(yield "unshift-spread")
+        )[yield "unshift-key"],
+        count = 0;
+        count < 1;
+        count++
+    ) {
+        yield "unshift-body-" + typeof value;
+    }
+    return "unshift-done";
+}
+
+const pushReceiver: any = ["push-first"];
+const pushSpread: any = ["push-spread-one", "push-spread-two"];
+const pushIterator = multiYieldPushNestedTemplateLogicalTreeArgumentAccessDeclarationInitializer();
+const pushFirst: any = pushIterator.next();
+console.log("push-first", pushReceiver.join(","), pushFirst.done, pushFirst.value);
+const pushSecond: any = pushIterator.next(pushReceiver);
+console.log("push-second", pushReceiver.join(","), pushSecond.done, pushSecond.value);
+const pushThird: any = pushIterator.next(true);
+console.log("push-third", pushReceiver.join(","), pushThird.done, pushThird.value);
+const pushFourth: any = pushIterator.next(true);
+console.log("push-fourth", pushReceiver.join(","), pushFourth.done, pushFourth.value);
+const pushFifth: any = pushIterator.next("push-tail-value");
+console.log("push-fifth", pushReceiver.join(","), pushFifth.done, pushFifth.value);
+const pushSixth: any = pushIterator.next("push-direct");
+console.log("push-sixth", pushReceiver.join(","), pushSixth.done, pushSixth.value);
+const pushSeventh: any = pushIterator.next(pushSpread);
+console.log("push-seventh", pushReceiver.join(","), pushSeventh.done, pushSeventh.value);
+const pushEighth: any = pushIterator.next("push-missing");
+console.log("push-eighth", pushReceiver.join(","), pushEighth.done, pushEighth.value);
+const pushDone: any = pushIterator.next();
+console.log("push-done", pushReceiver.join(","), pushDone.done, pushDone.value);
+
+const unshiftReceiver: any = ["unshift-first"];
+const unshiftSpread: any = ["unshift-spread-one", "unshift-spread-two"];
+const unshiftIterator = multiYieldUnshiftNestedTemplateLogicalTreeArgumentAccessDeclarationInitializer();
+const unshiftFirst: any = unshiftIterator.next();
+console.log("unshift-first", unshiftReceiver.join(","), unshiftFirst.done, unshiftFirst.value);
+const unshiftSecond: any = unshiftIterator.next(unshiftReceiver);
+console.log("unshift-second", unshiftReceiver.join(","), unshiftSecond.done, unshiftSecond.value);
+const unshiftThird: any = unshiftIterator.next(null);
+console.log("unshift-third", unshiftReceiver.join(","), unshiftThird.done, unshiftThird.value);
+const unshiftFourth: any = unshiftIterator.next(true);
+console.log("unshift-fourth", unshiftReceiver.join(","), unshiftFourth.done, unshiftFourth.value);
+const unshiftFifth: any = unshiftIterator.next("unshift-inner-right-value");
+console.log("unshift-fifth", unshiftReceiver.join(","), unshiftFifth.done, unshiftFifth.value);
+const unshiftSixth: any = unshiftIterator.next("unshift-tail-value");
+console.log("unshift-sixth", unshiftReceiver.join(","), unshiftSixth.done, unshiftSixth.value);
+const unshiftSeventh: any = unshiftIterator.next("unshift-direct");
+console.log("unshift-seventh", unshiftReceiver.join(","), unshiftSeventh.done, unshiftSeventh.value);
+const unshiftEighth: any = unshiftIterator.next(unshiftSpread);
+console.log("unshift-eighth", unshiftReceiver.join(","), unshiftEighth.done, unshiftEighth.value);
+const unshiftNinth: any = unshiftIterator.next("unshift-missing");
+console.log("unshift-ninth", unshiftReceiver.join(","), unshiftNinth.done, unshiftNinth.value);
+const unshiftDone: any = unshiftIterator.next();
+console.log("unshift-done", unshiftReceiver.join(","), unshiftDone.done, unshiftDone.value);
