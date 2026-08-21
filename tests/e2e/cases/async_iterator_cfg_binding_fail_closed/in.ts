b99c7856ae0ea9daa3bc62697b23ead3e8fc7d62
Adam Roberts <adam@intelligenta.uk>
2026-08-21 19:44:22 +0100

    Remove obsolete iterator binding rejection

--- tests/e2e/cases/async_iterator_cfg_binding_fail_closed/in.ts
+++
@@ -1,8 +0,0 @@
-async function unsupportedBindingSuspension(values: any): Promise<number> {
-    for await (const [value = await Promise.resolve(1)] of values) {
-        return value;
-    }
-    return 0;
-}
-
-unsupportedBindingSuspension([]).then(console.log);
